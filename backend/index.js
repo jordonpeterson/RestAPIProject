f641db6dad162612c658ea940b1dfa98dc82ea08
Jordon Peterson <jordonpropm@gmail.com>
2018-08-08 19:17:51 -0600

    Finished CRUD
    Added a put and delete method

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
 const express = require('express');
+
+
+
 const app = express();
+//Creates an express application called App
 app.use(express.json())
+//parses incoming requests with JSON payloads.
 
 values = [
     {id:1, name:"a"},
@@ -16,17 +21,14 @@ app.get('/users', (req,res) =>{
     res.send(values)
 })
 app.get('/users/:id', (req,res) =>{
-    const course = values.find(c => c.id ===parseInt(req.params.id));
-    if(!course) res.status(404).send('We can\t find that id. Sorry!');
-    res.send(course);
+    const value = values.find(c => {c.id ===parseInt(req.params.id)});
+    if(!value) res.status(404).send('We can\t find that id. Sorry!');
+    res.send(value);
 })
 
 app.post('/users', (req, res) => {
-    const schema = {
-        name: Joi.string().max(40).required()
-    };
-    const result = Joi.validate(req.body, schema)
-    console.log(result)
+   joiValidate(req.body)
+   // console.log(result)
     if(result.error){
         res.status(400).send(result.error.result)
         return;
@@ -36,6 +38,30 @@ app.post('/users', (req, res) => {
     name: req.body.name
     }
     values.push(value);
+    res.send(value);
+})
+const joiValidate = (input) =>{
+    const schema = {
+        name: Joi.string().max(40).required()
+    };
+    return Joi.validate(input, schema)
+}
+
+app.put('/users/:id',(req, res) => {
+    const value = values.find(c => c.id ===parseInt(req.params.id));
+    if(!value) res.status(404).send('We can\t find that id. Sorry!');
+
+    joiValidate(req.body);
+
+    value.name = req.body.name;
+    res.send(value)
+
+})
+app.delete('/users/:id', (req, res) => {
+    const value = values.find(c => c.id ===parseInt(req.params.id));
+    if(!value) res.status(404).send('We don\'t have a value with that id');
+    const index= values.indexOf(value);
+    values.splice(index,1);
     res.send(value);
 })
 
